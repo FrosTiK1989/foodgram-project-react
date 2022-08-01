1f786c9203a3b6c36cc391b44dfa76cb2f9ed588
Денис Маресов <frostik1989@yandex.ru>
2022-08-01 22:56:45 +0300

    first_part/try1

---
+++ backend/foodgram_project/foodgram_project/settings.py
@@ -0,0 +1,143 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET", default="any_key")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    "users",
+    "recipe",
+    "api",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "django_filters",
+    "rest_framework.authtoken",
+    "djoser",
+    "foodgram_project",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "foodgram_project.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "foodgram_project.wsgi.application"
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.getenv(
+            "DB_ENGINE", default="django.db.backends.postgresql"
+        ),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": "",
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+DOCS_DIR = os.path.join(BASE_DIR, "api/docs/")
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles/")
+
+AUTH_USER_MODEL = "users.FoodgramUser"
+
+REST_FRAMEWORK = {
+    "EXCEPTION_HANDLER": "api.utils.custom_exception_handler",
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend"
+    ],
+}
+
+
+DJOSER = {
+    "PERMISSIONS": {
+        "user_list": ["rest_framework.permissions.IsAuthenticatedOrReadOnly"],
+        "user": ["rest_framework.permissions.AllowAny"],
+    },
+    "SERIALIZERS": {
+        "user": "users.serializers.FoodgramUserSerializer",
+        "current_user": "users.serializers.FoodgramUserSerializer",
+        "user_create": "users.serializers.FoodgramUserCreateSerializer",
+    },
+    "HIDE_USERS": False,
+}
---
+++ backend/foodgram_project/users/serializers.py
@@ -0,0 +1,107 @@
+from djoser.serializers import UserCreateSerializer, UserSerializer
+from recipe.models import Recipe
+from rest_framework import serializers
+from rest_framework.generics import get_object_or_404
+
+from .models import Follow, FoodgramUser
+
+
+class FoodgramUserSerializer(UserSerializer):
+    is_subscibed = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = FoodgramUser
+        fields = (
+            "email",
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "is_subscribed",
+        )
+
+    def get_is_subscribed(self, following):
+        if self.context.get(
+            "reauest",
+        ).user.is_anonymous:
+            return False
+        return Follow.objects.filter(
+            user=self.context.get("request").user, following=following
+        ).exists()
+
+
+class FoodgramUserCreateSerializer(UserCreateSerializer):
+    class Meta:
+        model = FoodgramUser
+        fields = ("email", "username", "first_name", "last_name", "password")
+
+
+class FollowListSerializer(serializers.ModelSerializer):
+
+    recipes = serializers.SerializerMethodField()
+    recipes_count = serializers.SerializerMethodField()
+    is_subscribed = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = FoodgramUser
+        fields = (
+            "email",
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "is_subscribed",
+            "recipes",
+            "recipes_count",
+        )
+
+    def get_recipes_count(self, following):
+        return Recipe.objects.filter(author=following).count()
+
+    def get_recipes(self, following):
+        queryset = self.context.get("request")
+        recipes_limit = queryset.query_params.get("recipes_limit")
+        if not recipes_limit:
+            return RecipeFollowingSerializer(
+                following.author.all(),
+                many=True,
+                context={"request": queryset},
+            ).data
+        return RecipeFollowingSerializer(
+            following.author.all()[: int(recipes_limit)],
+            many=True,
+            context={"request": queryset},
+        ).data
+
+    def get_is_subscribed(self, following):
+        return Follow.objects.filter(
+            user=self.context.get("request").user, following=following
+        ).exists()
+
+
+class RecipeFollowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = ("id", "name", "image", "cooking_time")
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Follow
+        fields = ("user", "following")
+
+    def validate(self, data):
+        get_object_or_404(FoodgramUser, username=data["following"])
+        if self.context["request"].user == data["following"]:
+            raise serializers.ValidationError("Сам на себя подписываешься!")
+        if Follow.objects.filter(
+            user=self.context["request"].user, following=data["following"]
+        ):
+            raise serializers.ValidationError("Уже подписан")
+        return data
+
+    def to_representation(self, instance):
+        return FollowListSerializer(
+            instance.following,
+            context={"request": self.context.get("request")},
+        ).data
