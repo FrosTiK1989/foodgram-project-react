07260ea36bfedef04ee31f5b5e4e312aa665fd6b
Денис Маресов <frostik1989@yandex.ru>
2022-08-04 16:11:27 +0300

    rework project

--- backend/foodgram_project/api/paginator.py
+++
@@ -1,7 +0,0 @@
-from foodgram_project.settings import PAGE_SIZE
-from rest_framework.pagination import PageNumberPagination
-
-
-class FoodgramPagePagination(PageNumberPagination):
-    page_size = PAGE_SIZE
-    page_size_query_param = "limit"
