ad1b7465663bb971cf1361229c8b8c45f3c9571e
Денис Маресов <frostik1989@yandex.ru>
2022-08-06 14:07:33 +0300

    reback shopping card

--- backend/api/views.py
+++ backend/api/views.py
@@ -128,8 +128,8 @@ class RecipeViewSet(ModelViewSet):
         page.setFont('Handicraft', size=16)
         height = 750
         for i, (name, data) in enumerate(final_list.items(), 1):
-            page.drawString(75, height, (f'{data["recipe"]}:')
-                                        (f'{i}. {name} - {data["amount"]} '
+            page.drawString(75, height, (f'{data["recipe"]}:'
+                                         f'{i}. {name} - {data["amount"]} '
                                          f'{data["measurement_unit"]}'))
             height -= 25
         page.showPage()
